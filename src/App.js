ef6ceb82bfaa74b75ce62e08753b4b5120f1a3d2
SpiritON <Satanacamarro@gmail.com>
2022-09-11 21:38:06 +0300

    add styles with MUI, optimized component structure

--- src/App.js
+++ src/App.js
@@ -1,48 +1,34 @@
-import './App.css';
-import {useState} from "react";
-import InfoHeader from "./components/WeatherItem/Info";
-import WeatherForm from "./components/WeatherItem/WeatherForm";
-import WeatherResult from "./components/WeatherItem/WeatherResult";
+import WeatherForm from "./components/WeatherForm/WeatherForm";
+import WeatherResult from "./components/WeatherResult/WeatherResult";
+import GetWeatherRes from "./api/GetWeatherRes";
+import NavBar from "./components/NavBar/NavBar";
+import WeatherFooter from "./components/WeatherFooter/WeatherFooter";
+import { Box } from "@mui/system";
 
 
-const API_KEY = '9681983f6c4741e6bda182319220307'
+    function App() {
 
-function App() {
-    const [weatherData, setWeatherData] = useState('')
-    const gettingWeather = async (event) => {
-        event.preventDefault()
-        const city = event.target.elements.city.value
-        try {
-        const api_url = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`)
-        const data = await api_url.json()
-
-                setWeatherData({
-                    country: data.location.country,
-                    city: data.location.name,
-                    region: data.location.region,
-                    temp: data.current.temp_c,
-                    feelslike: data.current.feelslike_c,
-                    wind: data.current.wind_kph,
-                    condition: data.current.condition.text
-            })
-        }
-        catch (err) {
-            alert(' Cant get request from server ')
-        }
-
-
-    }
-
+    const {weatherData, getWeather, loading, error} = GetWeatherRes()
 
     return (
-        <>
-            <InfoHeader/>
-            <WeatherForm onSubmit={gettingWeather}/>
-            {weatherData.country ? (<WeatherResult data={weatherData}/>)
-            : (<div></div>)}
-
-
-        </>
+        <Box sx={{
+            overflow: 'hidden',
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100vh'
+        }}>  
+                <NavBar/>
+                <WeatherForm onSubmit={getWeather}/>
+                {weatherData && <WeatherResult data={weatherData}/>}
+                    <Box sx={{
+                        display: 'flex',
+                        margin: '0 auto'
+                        }}>
+                        {loading && <p>Loading...</p>}
+                        {error && <p>Can't getting data from server</p>}
+                    </Box>
+                <WeatherFooter/>
+        </Box>
     )
 }
 
